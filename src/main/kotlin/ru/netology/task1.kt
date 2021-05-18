92a903af48b96509079c6b963feb6fbe13e44e8d
tselishev <82764755+tselishev@users.noreply.github.com>
2021-05-18 19:43:08 +0500

    Изм. решение

--- src/main/kotlin/ru/netology/task1.kt
+++ src/main/kotlin/ru/netology/task1.kt
@@ -1,31 +1,26 @@
 package ru.netology
 
+import java.util.*
+
 fun main() {
+    val scanner = Scanner(System.`in`)
 
-    val transaction = 1000 // сумма перевода
-    val commission = 0.75 // комиссия 0,75%
-    val minTransaction = 35 // минимальная сумма перевода
-    val amount = 100 // количество копеек в 1 рубле
-
-    val minTransactionCop = minTransaction * amount // минимальная сумма перевода в копейках
-
-    val transactionCop = transaction * amount // сумма перевода в коп
-
-    val resultComission = transaction * commission / 100 // коммиссия с суммы перевода в руб
-        val resultComissionCop = transactionCop * commission / 100 // коммиссия с суммы перевода в коп
-
-    val resultTransaction = transaction - resultComission // итоговая сумма после снятия комиссии в руб
-    val resultTransactionCop = transactionCop - resultComissionCop // итоговая сумма после снятия комиссии в коп
-
-    if (transaction < minTransaction) {// итоговая сумма после снятия комиссии в руб
-        println("Минимальная сумма перевода составит: $minTransaction руб или  $minTransactionCop коп")
-    } else {
-        println("Сумма для перевода: $transaction руб ($transactionCop коп) " +
-                "\nКомиссия составит: $resultComission руб ($resultComissionCop коп) \n\nИтого сумма перевода, " +
-                "с учетом комиссии: $resultTransaction руб ($resultTransactionCop коп)")
-
-    }
+    println("Введите сумму перевода и нажмите 'enter':")
+    val transaction = scanner.nextInt()
+    val calcResult = calc(transaction)
+    println("Сумма перевода $transaction руб, комиссия: $calcResult руб")
 
 }
 
+fun calc(transaction: Int): Double {
+
+    val transactionCop = transaction * 100 // переводим в коп
+    var comissionCop = (transactionCop / 100) * 0.75
+    if (comissionCop < 3500) {
+        comissionCop = 3500.00
+    } else {
+        comissionCop
+    }
+    return comissionCop / 100 // переводим комиссию из коп в рубли
+}
 
